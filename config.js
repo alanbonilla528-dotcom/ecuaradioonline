fbb84217575d1b7e510c16baec9ecbac68323c94
alanbonilla528-dotcom <alanbonilla528@gmail.com>
2025-09-03 23:17:16 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const STATIONS = [
-  { name: "Ecua Radio",    url: "https://STREAM_URL_1", tagline: "La casa de la música ecuatoriana" },
-  { name: "Radio Amigo 1", url: "https://STREAM_URL_2", tagline: "Éxitos y novedades" },
+  { name: "Ecua Radio",    url:https://sonicpanel.streamsolutions.us/8010/stream tagline: "La casa de la música ecuatoriana" },
+  { name: Ecua Radio Online url:https://sonicpanel.streamsolutions.us/8026/stream tagline: musica variada },
   { name: "Radio Amigo 2", url: "https://STREAM_URL_3", tagline: "Clásicos y cultura" }
 ];
