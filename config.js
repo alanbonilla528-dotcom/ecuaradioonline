3c9a81121927cadab8738ed26e3301f160cf7fb5
alanbonilla528-dotcom <alanbonilla528@gmail.com>
2026-01-22 23:43:38 -0600

    Fix single radio stream

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const STATIONS = [
-  { name: "Ecua Radio",    url:https://sonicpanel.streamsolutions.us/8010/stream tagline: "La casa de la música ecuatoriana" },
-  { name: Ecua Radio Online url:https://sonicpanel.streamsolutions.us/8026/stream tagline: musica variada },
-  { name: "Radio Amigo 2", url: "https://STREAM_URL_3", tagline: "Clásicos y cultura" }
+  {
+    name: "Ecua Radio Online",
+    url: "https://sonicpanel.streamsolutions.us/8010/stream",
+    tagline: "La casa de la música ecuatoriana"
+  }
 ];
